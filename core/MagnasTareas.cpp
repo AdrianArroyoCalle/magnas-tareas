f86508a58b7db3415d91626c337f428205299c71
AdrianArroyoCalle <AdrianArroyoCalle@.(none)>
2015-02-16 14:52:07 +0100

    Usando std::vector<T>::insert en vez de devolver el Local Driver

--- core/MagnasTareas.cpp
+++ core/MagnasTareas.cpp
@@ -1,5 +1,6 @@
 #include "MagnasTareas.hpp"
 #include "../drivers/Local.hpp"
+#include <iostream>
 
 MagnasTareas::MagnasTareas()
 {
@@ -20,12 +21,13 @@ std::string MagnasTareas::GetVersion()
 
 std::vector<Category> MagnasTareas::GetCategories()
 {
-	/*std::vector<Category> vector;
+	std::vector<Category> vector;
 	if(localDriver->IsEnabled())
 	{
 		std::vector<Category> catDriver=localDriver->GetCategories();
-		vector.insert(catDriver.end(),catDriver.begin(),catDriver.end());
+		vector.reserve(vector.size() + catDriver.size());
+		vector.insert(vector.end(),catDriver.begin(),catDriver.end());
+		catDriver.clear();
 	}
-	return vector;*/
-	return localDriver->GetCategories(); 
+	return vector;
 }
